c52357ab5fa2490f38e2af6b0d00f80ddf770616
AnvilGit <67537752+AnvilGit@users.noreply.github.com>
2025-11-30 11:25:45 +0100

    Refactor user registration and logout functionality
    Updated the registration process to authenticate the user after registration and changed the redirect to a new template.

--- users/views.py
+++ users/views.py
@@ -1,25 +1,25 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
+from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
+
 def register(request):
-    """Register a new user."""
     if request.method != 'POST':
-        # Display blank registration form.
         form = UserCreationForm()
     else:
-        # Process completed form.
         form = UserCreationForm(data=request.POST)
-
         if form.is_valid():
             new_user = form.save()
-            # Log the user in and then redirect to home page.
-            login(request, new_user)
-            return redirect('learning_logs:index')
-
-    # Display a blank or invalid form.
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
     context = {'form': form}
-    return render(request, 'registration/register.html', context)
-
-
-
+    return render(request, 'users/user_registration.html', context)
