b63d0a627d6edbc5c2996b3297f6a800a85f2652
AnvilGit <67537752+AnvilGit@users.noreply.github.com>
2026-01-02 11:57:14 +0100

    Update logout view to use custom logout_view
    fixing logout - 405 error

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/userlogin.html'), name="login"),
-    path('logout/', LogoutView.as_view(template_name='users/userlogin.html'), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
+
 ]
